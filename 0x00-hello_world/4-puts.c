26483a5a16a13731830f5bdb353554ad1b2a21ad
Rahmon-Mojeed <rahmonmojeed99@gmail.com>
2022-09-09 07:40:57 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,4 @@
+#!/bin/bash
 #include <stdio.h>
 /**
  * main - main block
@@ -5,6 +6,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");	
+	printf("\"Programming is like building a multilingual puzzle\");	
 	return (0)
 }
