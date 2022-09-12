357d531bb1e07d651962d49701c95f0fd25d6a92
Rahmon-Mojeed <rahmonmojeed99@gmail.com>
2022-09-12 13:20:01 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,14 +6,14 @@
 */
 int main(void)
 {
-	int n = 48;
-	int l = 'a';
-	while (n <= 57)
+int n = 48;
+int l = 'a';
+	while(n <= 57)
 	{
 	putchar(n);
 	n += 1;
 	}
-	while (l <= 'f')
+	while(l <= 'f')
 	{
 	putchar(l);
 	l++;
