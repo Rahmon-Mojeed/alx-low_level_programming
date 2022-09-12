8bfbc008e5ffeca7a5fcfd444c13c7eb80939f7d
Rahmon-Mojeed <rahmonmojeed99@gmail.com>
2022-09-12 07:36:05 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -15,12 +15,12 @@
 */
 int main(void)
 {
-	int l = 'a';
-	while (l <= 'z')
-	{
-		putchar(l);
-		l += 1;
-	}
-	putchar('\n');
-	return (0);
+int l = 'a';
+while (l <= 'z')
+{
+putchar(l);
+l += 1;
+}
+putchar('\n');
+return (0);
 }
