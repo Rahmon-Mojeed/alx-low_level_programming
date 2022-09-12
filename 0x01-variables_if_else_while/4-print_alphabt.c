40db55955e7fa63965c51c242ea48eb1e4909904
Rahmon-Mojeed <rahmonmojeed99@gmail.com>
2022-09-12 07:50:11 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,15 +11,15 @@ int main(void)
 	int lw = 'a';
 	while (lw <= 'z')
 	{
-		if (lw == 'e' || lw == 'q')
-		{
-		lw += 1;
-		}
-		else
-		{
-		putchar(lw);
-		lw += 1;
-		}
+	if (lw == 'e' || lw == 'q')
+	{
+	lw += 1;
+	}
+	else
+	{
+	putchar(lw);
+	lw += 1;
+	}
 	}
 	putchar('\n');
 	return (0);
