29a9c7b443e5040bdfeea871a1bb68818b443804
Rahmon-Mojeed <rahmonmojeed99@gmail.com>
2022-09-20 12:31:15 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,18 +8,18 @@
 
 void rev_string(char *s)
 {
-	int i = 0;
-	int aux = 0;
-	char ltemp;
-	while (*(s + i) != '\0')
-		i += 1;
-	i -= 1;
-	while (aux < i)
-	{
-		ltemp = s[i];
-		s[i] = s[aux];
-		s[aux] = ltemp;
-		aux++;
-		i--;
-	}
+int i = 0;
+int aux = 0;
+char ltemp;
+while (*(s + i) != '\0')
+	i += 1;
+i -= 1;
+while (aux < i)
+{
+	ltemp = s[i];
+	s[i] = s[aux];
+	s[aux] = ltemp;
+	aux++;
+	i--;
+}
 }
