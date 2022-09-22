ff196a9b9c6f1be384ff892d72ae549846b26d5e
Rahmon-Mojeed <rahmonmojeed99@gmail.com>
2022-09-22 04:00:51 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,20 +8,20 @@
 
 char *leet(char *s)
 {
-int i = 0;
-int j = 0;
-char *l = "aAeEoOlLtT";
-char *n = "4433001177";
-while (*(s + i) != '\0')
-{
-	while (*(l + j) != '\0')
+	int i = 0;
+	int j = 0;
+	char *l = "aAeEoOlLtT";
+	char *n = "4433001177";
+	while (*(s + i) != '\0')
 	{
-		if (*(s + i) == *(l + j))
-																*(s + i)  = *(n + j);
-		j++;
+		while (*(l + j) != '\0')
+		{
+			if (*(s + i) == *(l + j))
+				*(s + i)  = *(n + j);
+			j++;
+		}
+		j = 0;
+		i++;
 	}
-	j = 0;
-	i++;
-}
-return (s);
+	return (s);
 }
