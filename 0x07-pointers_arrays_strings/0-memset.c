4d38f5cf349191c76ff5a95edef393afd625340d
Rahmon-Mojeed <rahmonmojeed99@gmail.com>
2022-09-27 04:39:10 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,11 +10,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
-	while (i < n)
-	{
-		*(s+i)= b;
-		i += 1;
-	}
-	return (s);
+unsigned int i = 0;
+while (i < n)
+{
+	*(s+i)= b;
+	i += 1;
+}
+return (s);
 }
