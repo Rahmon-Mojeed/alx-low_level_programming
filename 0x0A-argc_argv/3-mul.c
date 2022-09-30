3739171643630c93285ee8088e817ede55164deb
Rahmon-Mojeed <rahmonmojeed99@gmail.com>
2022-09-30 04:17:17 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,28 +10,28 @@
 
 int main(int argc, char *argv[])
 {
-	int count, num1, num2, res;
-	count = num1 = num2 = res = 0;
-	if (argc > 0)
+int count, num1, num2, res;
+count = num1 = num2 = res = 0;
+if (argc > 0)
+{
+	while (count < argc)
 	{
-		while (count < argc)
+		if (argc == 3)
 		{
-			if (argc == 3)
-			{
-				if (count == 1)
-					num1 = atoi(argv[count]);
-				else if (count == 2)
-					num2 = atoi(argv[count]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-			res = (num1 * num2);
-			count++;
+			if (count == 1)
+				num1 = atoi(argv[count]);
+			else if (count == 2)
+				num2 = atoi(argv[count]);
 		}
-		printf("%d\n", res);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		res = (num1 * num2);
+		count++;
 	}
-	return (0);
+	printf("%d\n", res);
+}
+return (0);
 }
