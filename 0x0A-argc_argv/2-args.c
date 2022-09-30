9ac44ed45ba980f587c4cc3396f4aae65d66f44b
Rahmon-Mojeed <rahmonmojeed99@gmail.com>
2022-09-30 04:13:25 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,14 +10,14 @@
 
 int main(int argc, char *argv[])
 {
-	int count = 0;
-	if (argc > 0)
+int count = 0;
+if (argc > 0)
+{
+	while (count < argc)
 	{
-		while (count < argc)
-		{
-			printf("%s\n", argv[count]);
-			count++;
-		}
+		printf("%s\n", argv[count]);
+		count++;
 	}
-	return (0);
+}
+return (0);
 }
