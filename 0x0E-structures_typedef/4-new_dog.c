6713ef8c60bf64bd7bd72e2b9706d6099786e3e0
Rahmon-Mojeed <rahmonmojeed99@gmail.com>
2022-10-11 05:24:35 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,23 +10,23 @@
 
 char *_strdup(char *str)
 {
-	char *dup;
-	unsigned int i = 0;
-	unsigned int j = 0;
-	if (str == NULL)
-		return (NULL);
-	while (str[i] != '\0')
-		i += 1;
+char *dup;
+unsigned int i = 0;
+unsigned int j = 0;
+if (str == NULL)
+	return (NULL);
+while (str[i] != '\0')
 	i += 1;
-	dup = malloc(i * sizeof(*dup));
-	if (dup == NULL)
-		return (NULL);
-	while (j < i)
-	{
-		dup[j] = str[j];
-		j += 1;
-	}
-		return (dup);
+i += 1;
+dup = malloc(i * sizeof(*dup));
+if (dup == NULL)
+	return (NULL);
+while (j < i)
+{
+	dup[j] = str[j];
+	j += 1;
+}
+return (dup);
 }
 
 /**
@@ -39,23 +39,23 @@ char *_strdup(char *str)
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *new_dog;
-	new_dog = malloc(sizeof(struct dog));
-	if (new_dog == NULL)
-		return (NULL);
-	new_dog->name = _strdup(name);
-	if (new_dog->name == NULL)
-	{
-		free(new_dog);
-		return (NULL);
-	}
-	new_dog->age = age;
-	new_dog->owner = _strdup(owner);
-	if (new_dog->owner == NULL)
-	{
-		free(new_dog);
-		free(new_dog->name);
-		return (NULL);
-	}
-	return (new_dog);
+struct dog *new_dog;
+new_dog = malloc(sizeof(struct dog));
+if (new_dog == NULL)
+	return (NULL);
+new_dog->name = _strdup(name);
+if (new_dog->name == NULL)
+{
+	free(new_dog);
+	return (NULL);
+}
+new_dog->age = age;
+new_dog->owner = _strdup(owner);
+if (new_dog->owner == NULL)
+{
+	free(new_dog);
+	free(new_dog->name);
+	return (NULL);
+}
+return (new_dog);
 }
