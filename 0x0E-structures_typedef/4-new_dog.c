ba285ff921089c767bf3463772de5b769e05b6d6
Rahmon-Mojeed <rahmonmojeed99@gmail.com>
2022-10-11 07:03:43 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,7 +17,7 @@ if (str == NULL)
 	return (NULL);
 while (str[i] != '\0')
 	i += 1;
-	i += 1;
+i += 1;
 dup = malloc(i * sizeof(*dup));
 if (dup == NULL)
 	return (NULL);
@@ -39,23 +39,23 @@ return (dup);
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-struct dog *new_dog;
-new_dog = malloc(sizeof(struct dog));
-if (new_dog == NULL)
-	return (NULL);
-new_dog->name = _strdup(name);
-if (new_dog->name == NULL)
-{
-	free(new_dog);
-	return (NULL);
-}
-new_dog->age = age;
-new_dog->owner = _strdup(owner);
-if (new_dog->owner == NULL)
-{
-	free(new_dog);
-	free(new_dog->name);
-	return (NULL);
-}
-return (new_dog);
+	struct dog *new_dog;
+	new_dog = malloc(sizeof(struct dog));
+	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = _strdup(name);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->age = age;
+	new_dog->owner = _strdup(owner);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog);
+		free(new_dog->name);
+		return (NULL);
+	}
+	return (new_dog);
 }
